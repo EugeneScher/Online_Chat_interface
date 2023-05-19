e56b970062b093e759497907fb3192cd4faa3346
Eugene <sher3456moon@gmail.com>
2023-05-19 16:55:10 +0700

    Second

---
+++ Chat2.0/Core/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using Chat2._0.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Chat2._0.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.CanExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
+
--- Chat2.0/MVVM/ViewModel/MainViewModel.cs
+++ Chat2.0/MVVM/ViewModel/MainViewModel.cs
@@ -4,13 +4,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using Chat2._0.MVVM.Model;
+using Chat2._0.Core;
 
 namespace Chat2._0.MVVM.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
 
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+
+        public ContactModel SelectedContact { get; set; }
+
+        private string _message;
+        
+        private string Message
+        {
+            get { return _message; }
+            set { _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FristMessage = false
+                });
+
+                Message = "";
+            });
+            
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://w7.pngwing.com/pngs/804/592/png-transparent-physical-education-school-educational-film-learning-people-icon-text-orange-logo.png",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FristMessage = false,
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Luxury",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://w7.pngwing.com/pngs/804/592/png-transparent-physical-education-school-educational-film-learning-people-icon-text-orange-logo.png",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true,
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://w7.pngwing.com/pngs/804/592/png-transparent-physical-education-school-educational-film-learning-people-icon-text-orange-logo.png",
+                Message = "Last",
+                IsNativeOrigin = false,
+                FristMessage = false,
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                { 
+                    Username = $"Allison {i}",
+                     ImageSource = "https://w7.pngwing.com/pngs/804/592/png-transparent-physical-education-school-educational-film-learning-people-icon-text-orange-logo.png",
+                     Messages = Messages
+                });
+        }
     }
 }
+}
+
