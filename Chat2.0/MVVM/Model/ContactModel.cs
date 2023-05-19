c35f0c7179c26955539c47d4564b44f6a0e5bc22
Eugene <sher3456moon@gmail.com>
2023-05-19 10:46:16 +0700

    frist upload

---
+++ Chat2.0/MVVM/Model/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Chat2._0.MVVM.Model
+{
+    class ContactModel
+    {
+        public string Username { get; set; }
+
+        public string ImageSource { get; set; }
+
+        public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public string LastMessage => Messages.Last().Message;
+    }
+}
---
+++ Chat2.0/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Chat2._0.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+    }
+}
